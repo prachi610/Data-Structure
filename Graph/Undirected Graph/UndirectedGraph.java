8771c1dc09f8d4b6c16f4b14717c4e8b7fb25a63
Prachi Singh <59611097+prachi610@users.noreply.github.com>
2021-11-05 19:05:10 +0530

    Update UndirectedGraph.java

--- Graph/Undirected Graph/UndirectedGraph.java
+++ Graph/Undirected Graph/UndirectedGraph.java
@@ -1,6 +1,9 @@
+
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class UndirectedGraph {
 	
@@ -17,6 +20,65 @@ public class UndirectedGraph {
 		A[dest].add(source);
 	}
 	
+	public void bfstry(int node, boolean[] visited) {
+		int source = 0;
+		visited[source] = true;
+
+		Queue<Integer> q = new LinkedList<>();
+		q.add(source);
+		while(!q.isEmpty()) {
+			
+			int cur = q.poll();
+			System.out.print(cur + " ");
+			for(int nieghbor : A[cur]) {
+				
+				if(!visited[nieghbor]) {
+					visited[nieghbor] = true;
+					q.add(nieghbor);
+				}
+				
+			}
+		}
+	}
+	
+	
+	public void bfs() {
+		boolean[] vis = new boolean[A.length];
+		for(int i = 0 ; i < A.length ; i++) {
+			if(!vis[i]) {
+				bfstry(i, vis);
+			}
+		}
+	}
+	
+
+	/********************* DFS traversal *************************/
+	public void dfs() {
+		boolean[] visited = new boolean[A.length];
+		for(int i = 0 ; i < A.length ; i++) {
+			if(!visited[i]) {
+				dfstry(i, visited);
+			}
+		}
+	}
+	
+	private void dfstry(int i, boolean[] visited) {
+		Stack<Integer> st = new Stack<>();
+		st.add(i);
+		visited[i] = true;
+		while(!st.isEmpty()) {
+			int store = st.pop();
+			System.out.print(store + " ");
+			for(int neighbor : A[store])
+			if(!visited[neighbor]) {
+				visited[neighbor] = true;
+				st.add(neighbor);
+			}
+			
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int v = sc.nextInt();
@@ -28,8 +90,6 @@ public class UndirectedGraph {
 			int dest = sc.nextInt();
 			graph.addEdge(source, dest);
 		}
-    
-    /******************* Printing Graph ********************/
 		for(int i = 0 ; i < v ; i++) {
 			System.out.print(i + " - ");
 			for(int j: graph.A[i]) {
@@ -39,5 +99,8 @@ public class UndirectedGraph {
 		}
 		System.out.println();
 		
+		graph.bfs();
+		System.out.println();
+		graph.dfs();
 	}
 }
